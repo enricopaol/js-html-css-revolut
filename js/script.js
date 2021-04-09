a33b21b6c5c772a83faf04987e11f8d81dcaaa05
Enrico Paolazzi <paolazzienrico@gmail.com>
2021-04-09 19:31:09 +0200

    Terminato menu no responsive

--- js/script.js
+++ js/script.js
@@ -3,10 +3,28 @@ $(document).ready(function() {
     // All'evento mouseenter di li, devo aggiungere la classe show al figlio .dropdown 
 
     $('.menu li').mouseenter(function() {    
-        $(this).children('.dropdown').addClass('show');
+        $(this).children('.dropdown').addClass('show'); 
+
+        if (!$(this).hasClass('btn')) {
+            $(this).children('a').css('color', '#030303');  
+        }        
+
+        if ($(this).hasClass('icon')) {
+            $('.icon i').removeClass('fa-chevron-down').addClass('fa-chevron-up');
+        }
     });
 
     $('.menu li').mouseleave(function() {
         $(this).children('.dropdown').removeClass('show');
+
+        
+        if (!$(this).hasClass('btn')) {
+            $(this).children('a').css('color', '#a8aeb3');
+        }
+
+        if ($(this).hasClass('icon')) {
+            $('.icon i').removeClass('fa-chevron-up').addClass('fa-chevron-down');
+        }
     });
+
 });
