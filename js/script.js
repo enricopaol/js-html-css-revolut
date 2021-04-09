2721fc8bf8b3b8c30533a85343a1c70d6872ab19
Enrico Paolazzi <paolazzienrico@gmail.com>
2021-04-09 17:34:31 +0200

    Realizzata prima dropdown

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
 $(document).ready(function() {
 
+    // All'evento mouseenter di li, devo aggiungere la classe show al figlio .dropdown 
+
+    $('.menu li').mouseenter(function() {
     
+        $(this).children('.dropdown').removeClass('hide').addClass('show');
+    });
+
+    $('.menu li').mouseleave(function() {
+        $(this).children('.dropdown').removeClass('show').addClass('hide');
+    });
 });
