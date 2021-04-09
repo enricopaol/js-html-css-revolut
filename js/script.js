ecf8c814ff00e3db5931cf25002dcad885d2716d
Enrico Paolazzi <paolazzienrico@gmail.com>
2021-04-09 18:25:18 +0200

    Risolto display dropdown

--- js/script.js
+++ js/script.js
@@ -2,12 +2,11 @@ $(document).ready(function() {
 
     // All'evento mouseenter di li, devo aggiungere la classe show al figlio .dropdown 
 
-    $('.menu li').mouseenter(function() {
-    
-        $(this).children('.dropdown').removeClass('hide').addClass('show');
+    $('.menu li').mouseenter(function() {    
+        $(this).children('.dropdown').addClass('show');
     });
 
     $('.menu li').mouseleave(function() {
-        $(this).children('.dropdown').removeClass('show').addClass('hide');
+        $(this).children('.dropdown').removeClass('show');
     });
 });
