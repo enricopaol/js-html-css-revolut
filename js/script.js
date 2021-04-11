49dfa829184a37796f3619119ff5c0ffdaf941d2
Enrico Paolazzi <paolazzienrico@gmail.com>
2021-04-11 19:57:10 +0200

    Aggiunto sliding menu responsive

--- js/script.js
+++ js/script.js
@@ -27,4 +27,33 @@ $(document).ready(function() {
         }
     });
 
+
+    // Dropdow Burger Menu
+    $('.dropdown-icon').click(function() {
+        $(this).siblings('.dropdown-burger').slideToggle(500);
+
+        if ($(this).children('i').hasClass('fa-chevron-down')) {
+            $(this).children('i').removeClass('fa-chevron-down').addClass('fa-chevron-up');
+        } else if ($(this).children('i').hasClass('fa-chevron-up')) {
+            $(this).children('i').removeClass('fa-chevron-up').addClass('fa-chevron-down');
+        }        
+    });
+
+    // Slide Left Burger Menu
+    $('.burger > a').click(function() {
+        $('.slide-menu-container').addClass('active');
+    });
+
+    $('.exit-icon').click(function() {
+        $('.slide-menu-container').removeClass('active');
+    });
+
+    // Toccare ovunque per chiudere il menu
+    $(document).click(function(e) {
+        var target = $(e.target);
+        if (target.is(".burger > a i, .slide-menu-container, .slide-menu, li, li a, li span, li span i, .menu-dropdown-burger, .button") === false) {
+          $('.slide-menu-container').removeClass("active");          
+        }
+    });
+
 });
